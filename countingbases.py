ad92402bcea6177927deb1278978928ad4499a8f
Aman Pruthi <54753316+amanpruthi15@users.noreply.github.com>
2020-02-17 10:59:45 -0600

    Add files via upload

---
+++ countingbases.py
@@ -0,0 +1,17 @@
+import random
+bases = ["A","C","T","G"]
+sequence = random.choices(bases,k=100)
+print(sequence)
+
+G_string = sequence.count('G')
+A_string = sequence.count('A')
+A_and_C_string = sequence.count('A') + sequence.count('C')
+
+string1 = "Number of G's = "
+string2 = "Number of A's = "
+string3 = "Number of A's + C's = "
+
+print (string1 + str(G_string))
+print (string2 + str(A_string))
+print (string3 + str(A_and_C_string))
+
