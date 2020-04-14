bd427e386d24793d03a2f5d3723992a45d244f77
Aman Pruthi <54753316+amanpruthi15@users.noreply.github.com>
2020-04-14 15:40:47 -0500

    Add files via upload

---
+++ genecomprehension.py
@@ -0,0 +1,43 @@
+# Aman Pruthi
+# genes.py
+# 4 April, 2020
+# Finding the length of the genes
+
+#########################
+# Basic Steps
+#########################
+# 1. Generating a random RNA sequence of 1000 bp
+# 2. Create a list genes starting from AUG
+# 3. Create a list of large genes
+# 4. Determine the length of large genes
+
+#########################
+# Starting the command line
+#########################
+
+#########################
+# 1. Generating a random RNA sequence of 1000 bp
+########################
+import random
+bases = ['A', 'U', 'G', 'C']
+RNA = random.choices(bases, k=1000)
+mRNA = ''.join(RNA)
+print('\nRandomly generated mRNA seqeunce is:\n\n',mRNA)
+
+#########################
+# 2. Create a list genes starting from AUG
+#########################
+split = mRNA.split('AUG')
+print("\nIf we split the sequence at every start codon i.e. AUG the genes that will be formed are:\n\n",split)
+length = len(split)
+print ('\nThe number of genes formed are:',length)
+
+########################
+# 3. Create a list of large genes
+########################
+largegenes=[x for x in split if len(x)>50]
+print("Genes greater than 50 bp are:\n",largegenes)
+print("######################")
+print("\nThe length of the large genes are:\n")
+for a in largegenes:
+	print(len(a))
